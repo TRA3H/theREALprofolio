1a69eaaaec8aec4f503855225a282cd6a5093d10
Cyrus Baybay <41849568+TRA3H@users.noreply.github.com>
2022-09-13 17:45:22 -0700

    apple-laptop
    mhm

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+//TODO need to install react package to create an About me page
+//TODO wrap the list items as links so we can have a multi page website
+//TODO need to make the list items on the left
+//TODO we need to make it responsive to mobile
+//TODO if mobile then we need to make the nav bar condensed three bars
+
 function Nav() {
   return (
     <div className="nav-container">
       <ul>
-        <li className="nav_items">About Me</li>
+        <li className="nav_items">About me</li>
         <li className="nav_items">Contact</li>
       </ul>
     </div>
